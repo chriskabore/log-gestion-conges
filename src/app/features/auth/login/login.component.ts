616fb400406690207d32b0e1cc467520d3c33396
Sidnooma Christian KABORE <chriskabor@gmail.com>
2024-06-12 23:34:37 +0000

    latest commit

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -4,13 +4,14 @@ import { Login } from '../../../models/login.model';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit {
-
+  
   formLogin!: FormGroup
   login: Login = new Login();
   constructor(
